1f3c1cdc49d6bad62307e4cd62084eb969f34885
noeliarodriguez8 <noeliarodriguez888@hotmail.com>
2024-06-10 01:26:09 +0200

    add modal post and fetch add comments

--- FeedScreen/FeedScreen.js
+++ FeedScreen/FeedScreen.js
@@ -1,6 +1,13 @@
-// import { convertToBase64 } from "../Utils/Utils.js"
-// import { API_BASE_URL } from "../config.js"
-const getPosts = async (postID) => {
+const modalAdd = document.querySelector("#modalAdd")
+const closeModalBtn = document.querySelector("#closeModalBtn")
+const idPost = document.getElementById("idPost")
+const imagenPost = idPost.querySelector("img")
+const username = document.getElementById("user")
+const comments = document.getElementById("idComment")
+const idLike = document.getElementsByTagName("#idLike")
+const inputComment = document.getElementById("inputComment")
+const btnAddComment = document.getElementById("btnAddComment")
+const getPosts = async () => {
   try {
     const response = await fetch(
       "http://192.168.1.86:9000/memeo/api/getposts/4"
@@ -9,7 +16,7 @@ const getPosts = async (postID) => {
       throw new Error("Network response was not ok " + response.statusText)
     }
     const dataPost = await response.json()
-    showPosts(dataPost, postID)
+    showPosts(dataPost)
   } catch (error) {
     console.error("Error fetching posts:", error)
   }
@@ -24,9 +31,7 @@ const changeImgLiked = (post) => {
   }
 }
 const isLiked = (post) => {
-  debugger
-  let isLike = post.memeLikes.some((like) => like.memeLikePK.userID === 4)
-  return isLike
+  return post.memeLikes.some((like) => like.memeLikePK.userID === 4)
 }
 const formatDatePost = (date) => {
   const dateFormat = new Date(date)
@@ -41,16 +46,15 @@ const formatDatePost = (date) => {
 
   return `${formattedDate} ${formattedTime}`
 }
-const showPosts = (dataPost, postID) => {
+const showPosts = (dataPost) => {
   const lista = document.getElementById("listPost")
   removeChildNodes(lista)
   dataPost.forEach(async (elemento) => {
     const postDiv = document.createElement("article")
     postDiv.classList.add("post")
-    debugger
 
     let imgPost = `http://192.168.1.86:9000/${elemento.media_file}`
-    debugger
+
     const postContent = `
       <figure>
         <img class="img-post" id="post-${
@@ -66,7 +70,9 @@ const showPosts = (dataPost, postID) => {
           </span>
           <span>
             ${elemento.comments.length}
-            <img class="heart-icon" src="../Assets/comments-icon.svg"/>
+            <img class="heart-icon" id="comment-post-${
+              elemento.postID
+            }" src="../Assets/comments-icon.svg"/>
           </span>
         </div>
         <div class="details-section">
@@ -98,22 +104,29 @@ const showPosts = (dataPost, postID) => {
   //Lógica darle a like
   dataPost.forEach((elemento) => {
     const btnLike = document.getElementById(`btnLike-${elemento.postID}`)
+    const btnImg = document.getElementById(`post-${elemento.postID}`)
+    const btnComments = document.getElementById(
+      `comment-post-${elemento.postID}`
+    )
     btnLike.addEventListener("click", function () {
-      debugger
       const newImgSrc = isLiked(elemento)
         ? "../Assets/no-like-icon.png"
         : "../Assets/like-icon.png"
       btnLike.classList.add("like")
-      if (!isLiked(elemento)) {
-        createMemeLike(elemento)
-      } else {
-        deleteMemeLike(elemento.postID)
-      }
       setTimeout(() => {
         btnLike.src = newImgSrc
         btnLike.classList.remove("like")
       }, 200)
+      setTimeout(() => {
+        if (!isLiked(elemento)) {
+          createMemeLike(elemento)
+        } else {
+          deleteMemeLike(elemento.postID)
+        }
+      }, 200)
     })
+    btnImg.addEventListener("click", () => openModal(elemento))
+    btnComments.addEventListener("click", () => openModal(elemento))
   })
 }
 const createMemeLike = async (post) => {
@@ -166,5 +179,73 @@ const removeChildNodes = (parent) => {
     parent.removeChild(parent.firstChild)
   }
 }
+const openModal = (post) => {
+  modalAdd.style.display = "flex"
+  username.textContent = post.user.username
+  imagenPost.src = `http://192.168.1.86:9000/${post.media_file}`
+  loadComments(post)
+}
 
-document.addEventListener("DOMContentLoaded", getPosts())
+const addCommentHandler = (post) => () => addComment(post)
+
+const closeModal = () => {
+  modalAdd.style.display = "none"
+  getPosts()
+}
+
+//para guardar el evento cuando se crea
+let currentAddCommentHandler
+
+const loadComments = (post) => {
+  comments.innerHTML = ""
+  post.comments.forEach((comment) => {
+    const commentSpan = document.createElement("span")
+    const commentContent = `<strong>${comment.user.username}</strong> ${comment.text_content}`
+    commentSpan.innerHTML = commentContent
+    comments.appendChild(commentSpan)
+  })
+  inputComment.value = ""
+  // eliminar el event listener anterior si existe
+  if (currentAddCommentHandler) {
+    btnAddComment.removeEventListener("click", currentAddCommentHandler)
+  }
+  currentAddCommentHandler = addCommentHandler(post)
+  btnAddComment.addEventListener("click", currentAddCommentHandler)
+}
+
+const addComment = async (post) => {
+  const comment = {
+    text_content: inputComment.value,
+    user: {
+      userID: 4,
+      username: "theking",
+    },
+    post: {
+      postID: post.postID,
+    },
+  }
+  try {
+    const response = await fetch(
+      `http://192.168.1.86:9000/memeo/api/createcomment`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(comment),
+      }
+    )
+    if (!response.ok) {
+      throw new Error("Network response was not ok " + response.statusText)
+    }
+  } catch (error) {
+    console.error("Error fetching posts:", error)
+  }
+  post.comments.push(comment)
+  loadComments(post)
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  closeModalBtn.addEventListener("click", () => closeModal())
+  getPosts()
+})
