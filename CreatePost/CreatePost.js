4b546fac5b63ef2b84acb47ae09afb729076628b
pSand99 <patrisand5800@gmail.com>
2024-06-12 16:31:34 +0200

    añadido (creo que bien) el sessionStorage en createPost y en conversations

--- CreatePost/CreatePost.js
+++ CreatePost/CreatePost.js
@@ -8,45 +8,34 @@ document.addEventListener('DOMContentLoaded', () =>{
         //esta deprecado
         event.preventDefault(); 
 
-         const postText = document.getElementById("postText").value;
+        const user = JSON.parse(sessionStorage.getItem("user"));
+        if(!user){
+            console.error("No se han encontrado datos de sesion de este usuario :(");
+            return;
+        }
+
+
+        const postText = document.getElementById("postText").value;
         const postImg = document.getElementById("postImg").files[0];
+        
+        if(!postImg){
+            console.error("Te faltó el meme...");
+            return;
+        }
 
-        if (postImg) {
-            const reader = new FileReader();
-            reader.onloadend = function () {
-                const base64String = reader.result
-                    .replace("data:", "")
-                    .replace(/^.+,/, "")
+        const reader = new FileReader();
+        reader.onloadend = function () {
+            const base64String = reader.result
+                .replace("data:", "")
+                .replace(/^.+,/, "")
 
-                const post = {
-                    text_content: postText,
-                    user: {
-                        userID: 1,
-                        username: "theking",
-                    },
-                    media_file: base64String,
-                }
-                fetch("http://192.168.56.1:9000/memeo/api/createpost", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                        "Access-Control-Allow-Origin": "http://192.168.56.1:9000",
-                    },
-                    body: JSON.stringify(post),
-                })
-                    .then((response) => response.json())
-                    .then((data) => console.log("Success:", data))
-                    .catch((error) => console.error("Error:", error))
-            }
-            reader.readAsDataURL(postImg);
-        } else if (postText) {
             const post = {
                 text_content: postText,
                 user: {
-                    userID: 1,
-                    username: "theking",
+                    userID: user.userID,
+                    username: user.username,
                 },
-                media_file: "",
+                media_file: base64String,
             }
             fetch("http://192.168.56.1:9000/memeo/api/createpost", {
                 method: "POST",
@@ -60,6 +49,7 @@ document.addEventListener('DOMContentLoaded', () =>{
                 .then((data) => console.log("Success:", data))
                 .catch((error) => console.error("Error:", error))
         }
-    })
+        reader.readAsDataURL(postImg);
+})
     
 })
