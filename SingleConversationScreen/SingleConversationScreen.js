b3dff8c7eb10c8869addf314916fe5649df7043a
noeliasr <noelia.srfraile@gmail.com>
2024-06-14 14:43:21 +0200

    pantalla single conversation 75% funcionalidad

--- SingleConversationScreen/SingleConversationScreen.js
+++ SingleConversationScreen/SingleConversationScreen.js
@@ -1,124 +1,108 @@
 // para asegurarnos de que carga antes de tirar el evento
-document.addEventListener('DOMContentLoaded', (event) => {
+document.addEventListener('DOMContentLoaded', () => {
 
     async function fetchUserData() {
         const loggedUser = JSON.parse(sessionStorage.getItem("user"))
         const url = `http://localhost:9000/memeo/api/getconversations/${loggedUser.userID}`;
-        // const url = `http://localhost:9000/memeo/api/getconversations/${userID}`;
 
         //qué conversación es?
-        // const params = new URLSearchParams(window.location.search);
-        // const conversationID = params.get('conversationID');
-
-        var conversationID = 0; //hardcoded
-
+        const params = new URLSearchParams(window.location.search);
+        const conversationIDReceived = params.get('conversationID');
 
         try {
             const response = await fetch(url);
-
             if (!response.ok) {
                 throw new Error(`Error en la solicitud: ${response.status}`);
             }
 
             const dataConversation = await response.json();
-            
             if(!dataConversation){
-                console.log("No está cogiendo conversaciones.")
+                console.error("No está cogiendo conversaciones.")
             } else{
-                console.log("Sí coge conversaciones.")
-                console.log(dataConversation);
+                console.log("Sí coge conversaciones:")
+                console.log(dataConversation)
 
-                // setteo de variables: persona con la que se mantiene la conversación
-                var conversationWithUser = document.querySelector(".conversationWith");
-                
-                // lógica para saber cuál es el usuario sender-receiver//loggeado-remitente
-                if(dataConversation[conversationID].conversationPK.starterUserID == loggedUser.userID){ // userID del usuario loggeado
-                // if(dataConversation[conversationID].conversationPK.starterUserID == 4){ // hardcoded
+                var dataConversationSelection = null
+
+                dataConversation.forEach(conversation => {
                     try {
-                        const response = await fetch(`http://localhost:9000/memeo/api/getuser/${dataConversation[conversationID].conversationPK.receiverUserID}`);
-                        
-                        if (!response.ok) {
-                            throw new Error(`Error en la solicitud: ${response.status}`);
+                        if(conversation.conversationPK.conversationID == conversationIDReceived){
+                            console.log("Yes, conversation.conversationID: " + conversation.conversationPK.conversationID + " y " + "conversationIDReceived: " + conversationIDReceived)
+                            dataConversationSelection = conversation
+    
+                            // console.log("Hemos encontrado y lo guardamos en dataConversationSelection: ")
+                            // console.log(conversation)
                         }
-
-                        const dataUserTitle = await response.json();
-                        conversationWithUser.textContent = dataUserTitle.username;
                     } catch (error) {
-                        console.error("Error en la petición del dataUserTitle para el título de la conversación (1)")
-                        console.error('ERROR REQUEST FETCH:', error);
+                        console.error("No corresponde: conversation.conversationID ningún conversationIDReceived (?)")
+                        console.error('Sent error:', error);
                     }
+                });
 
-                } else{
-                    console.log("debería de salir si si")
-                    try {
-                        const response = await fetch(`http://localhost:9000/memeo/api/getuser/${dataConversation[conversationID].conversationPK.starterUserID}`);
-                        
-                        if (!response.ok) {
-                            throw new Error(`Error en la solicitud: ${response.status}`);
-                        }
+                //necesitamos saber quién es el starterConversation y el recieverConversation
+                if(dataConversationSelection.conversationPK.starterUserID == loggedUser.userID){ // usuario loggeado es el que inició la conversación 
+                    if(dataConversationSelection.directMessages == 0){
+                        console.log("No hay mensajes.");
+    
+                        var noMensajes = document.createElement("div");
+                        noMensajes.textContent = "No hay mensajes que mostrar :("
+                        msgContainer.appendChild(noMensajes); //FALTA ESTILO
+    
+                    } else{
+                        console.log("Hay mensajes.");
 
-                        const dataUserTitle = await response.json();
-                        conversationWithUser.textContent = dataUserTitle.username;
-                    } catch (error) {
-                        console.error("Error en la petición del dataUserTitle para el título de la conversación (2)")
-                        console.error('ERROR REQUEST FETCH:', error);
-                    }
-                }
+                        try {
+                            const response = await fetch(`http://localhost:9000/memeo/api/getdms/${dataConversationSelection.conversationPK.conversationID}`);
 
-            }
-
-
-            // settear mensajes
-            var msgContainer = document.querySelector('.messagesContainer');
-
-            if(dataConversation[conversationID].directMessages == 0){
-                console.log("No hay mensajes.");
-
-                var noMensajes = document.createElement("div");
-                noMensajes.textContent = "No hay mensajes que mostrar :("
-                msgContainer.appendChild(noMensajes); //FALTA ESTILO
-
-            }else{
-                console.log("Hay mensajes.");
-
-                try {
-                    const response = await fetch(`http://localhost:9000/memeo/api/getdms/${conversationID}`);
-
-                    if (!response.ok) {
-                        throw new Error(`Error en la solicitud: ${response.status}`);
-                    }
-            
-                    const dataMessages = await response.json();
-                    console.log(dataMessages); //dataMessages ya es un array
+                            if (!response.ok) {
+                                throw new Error(`Error en la solicitud: ${response.status}`);
+                            }
                     
-                    dataMessages.forEach( (dm)=> {
-                        if(dataMessages.senderUser.userID[dm] === loggedUser.userID){
-                            //se crea y va a la derecha
-                            var containerMessage = document.createElement("div");
-                            containerMessage.classList.add('sent');
-                            containerMessage.textContent = `${dataMessages.text_content}`;
-                            msgContainer.appendChild(containerMessage);
-                        } else{
-                            //se crea y va a la izquierda
-                            var containerMessage = document.createElement("div");
-                            containerMessage.classList.add('received');
-                            containerMessage.textContent = `${dataMessages.text_content}`;
-                            msgContainer.appendChild(containerMessage);
+                            const dataMessages = await response.json();
+                            console.log("DATA MESSAGES:")
+                            console.log(dataMessages); //dataMessages ya es un array
+
+                            // settear mensajes
+                            var msgContainer = document.querySelector('.messagesContainer');
+                            
+                            dataMessages.forEach( (dm)=> { //dm es el objeto mensaje
+                                if(dm.senderUser.userID === loggedUser.userID){
+                                    //se crea y va a la derecha
+                                    var containerMessage = document.createElement("div");
+                                    containerMessage.classList.add('sent');
+                                    containerMessage.textContent = `${dm.text_content}`;
+                                    msgContainer.appendChild(containerMessage);
+                                } else{
+                                    //set conversation title
+                                    var conversationWithTitle = document.querySelector(".conversationWith");
+                                    conversationWithTitle.textContent = dm.senderUser.username
+
+                                    //se crea y va a la izquierda
+                                    var containerMessage = document.createElement("div");
+                                    containerMessage.classList.add('received');
+                                    containerMessage.textContent = `${dm.text_content}`;
+                                    msgContainer.appendChild(containerMessage);
+
+
+                                }
+                            });
+    
+                        } catch (error) {
+                            console.error('ERROR REQUEST FETCH:', error);
                         }
-                    });
-
-                } catch (error) {
-                    console.error("Error en el fetch de los mensajes directos (1)");
-                    console.error('ERROR REQUEST FETCH:', error);
+                    }
+                } else{ //qué sucede cuando el starter no es el loggeado
+                    
                 }
-
+                
+                return dataConversation;
             }
-
-            return dataConversation;
-
+        
         } catch (error) {
+            console.error("Error en el fetch de los mensajes directos (1)");
             console.error('ERROR REQUEST FETCH:', error);
         }
     }
+
     fetchUserData();
 });
